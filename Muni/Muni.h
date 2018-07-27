c957172c32da37ab7bef2b5c275993778c9a59fb
Norikazu Muramoto <tmy0x3@icloud.com>
2018-07-27 14:06:56 +0900

    Initial Commit

---
+++ Muni/Muni.h
@@ -0,0 +1,19 @@
+//
+//  Muni.h
+//  Muni
+//
+//  Created by 1amageek on 2018/07/27.
+//  Copyright © 2018年 1amageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Muni.
+FOUNDATION_EXPORT double MuniVersionNumber;
+
+//! Project version string for Muni.
+FOUNDATION_EXPORT const unsigned char MuniVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Muni/PublicHeader.h>
+
+
---
+++ MuniTests/MuniTests.swift
@@ -0,0 +1,36 @@
+//
+//  MuniTests.swift
+//  MuniTests
+//
+//  Created by 1amageek on 2018/07/27.
+//  Copyright © 2018年 1amageek. All rights reserved.
+//
+
+import XCTest
+@testable import Muni
+
+class MuniTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
