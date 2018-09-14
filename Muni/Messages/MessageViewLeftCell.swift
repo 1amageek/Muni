a31320ec06fd4ef356c7755c79c45d1d56e264e1
1amageek <tmy0x3@icloud.com>
2018-09-14 17:50:27 +0900

    bugfix

--- Muni/Messages/MessageViewLeftCell.swift
+++ Muni/Messages/MessageViewLeftCell.swift
@@ -16,7 +16,7 @@ open class MessageViewLeftCell: MessageViewCell {
     @IBOutlet public weak var balloonView: UIView!
     @IBOutlet public weak var dateLabel: UILabel!
 
-    public override func awakeFromNib() {
+    open override func awakeFromNib() {
         super.awakeFromNib()
         self.thumbnailImageView.layer.cornerRadius = 12
         self.balloonView.layer.cornerRadius = 16
@@ -25,7 +25,7 @@ open class MessageViewLeftCell: MessageViewCell {
         self.dateLabel.text = nil
     }
 
-    public override func prepareForReuse() {
+    open override func prepareForReuse() {
         super.prepareForReuse()
         self.titleLabel.text = nil
         self.textLabel.text = nil
